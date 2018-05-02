806d6afb23b0351dba2cd2dc999eed0d5b9b4926
hengwu <youchuangqiba@163.com>
2018-05-03 02:18:03 +0800

    upd8

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ alert('This component is being loaded!');
 export default function (targetElement, options) {
     /* Do some stuff as the component DOM is ready */
 
-    alert('This component\'s DOM is ready!', targetElement);
+    alert('This component DOM is ready!');
 
     targetElement.querySelector('img').addEventListener('click', function (e) {
         alert(`hello ${options.hello}`);
